f60a6332327bca8b882da75b28b37385767f1fde
spectorari <spectorari@gmail.com>
2023-02-25 13:12:29 -0500

    commit #2
    includes:
      - inheritance package
          - Shapes class
          - Triangle class
          - TwoDShape class

---
+++ JavaTutorials/src/inheritance/Triangle.java
@@ -0,0 +1,15 @@
+package inheritance;
+//Hierarchy example from Java: A Beginner's Edition, Herbert Schildt (Chapter 7, pp. 230 - 232)
+
+// A subclass of TwoDShape for triangles.
+public class Triangle extends TwoDShape {
+	String style;
+	
+	double area() {
+		return width * height / 2;
+	}
+	
+	void showStyle() {
+		System.out.println("Triangle is " + style);
+	}
+}
---
+++ JavaTutorials/src/inheritance/TwoDShape.java
@@ -0,0 +1,12 @@
+package inheritance;
+// Hierarchy example from Java: A Beginner's Edition, Herbert Schildt (Chapter 7, pp. 230-232)
+
+// A simple class hierarchy
+public class TwoDShape {
+	double width;
+	double height;
+	
+	void showDim() {
+		System.out.println("Width and height are " + width + " and " + height);
+	}
+}
